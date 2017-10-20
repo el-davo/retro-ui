2e73bd8d31ffc8b18877366d6c2deac08cfb7d1a
ahernd2 <david.ahern@dell.com>
2017-10-20 21:56:02 +0100

    Added redux forms module

--- src/app/nav/nav.state.ts
+++ src/app/nav/nav.state.ts
@@ -1,11 +1,25 @@
 export const navState: NavState = {
   showLoginModal: false,
   showSignupModal: false,
-  isSigningUp: false
+  isSigningUp: false,
+  signupForm: {
+    username: '',
+    email: '',
+    password: '',
+    passwordRetype: ''
+  }
 };
 
 export interface NavState {
   showLoginModal: boolean;
   showSignupModal: boolean;
   isSigningUp: boolean;
+  signupForm: SignupForm;
+}
+
+export interface SignupForm {
+  username: string;
+  email: string;
+  password: string;
+  passwordRetype: string;
 }
--- src/app/root.reducer.ts
+++ src/app/root.reducer.ts
@@ -1,8 +1,12 @@
 import {routerReducer as router} from '@angular-redux/router';
 import {combineReducers} from 'redux';
 import {navReducer as nav} from './nav/nav.reducer';
+import {composeReducers, defaultFormReducer} from '@angular-redux/form';
 
-export const rootReducer = combineReducers({
-  router,
-  nav
-});
+export const rootReducer = composeReducers(
+  defaultFormReducer(),
+  combineReducers({
+    router,
+    nav
+  })
+);
